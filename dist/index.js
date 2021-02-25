9a06ee1a113327c3cf9b09b9b370f85670b983ad
Steve Thompson <writetome51@gmail.com>
2021-02-24 20:17:37 -0700

    new commit

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
 import {get_ymd_hms} from './privy.js';
 
 
-export function get_ymd_hms_local(date, options = {includeFullYear: false} // if false, year has 2 digits
+export function get_ymd_hms_local(
+	date, options = {includeFullYear: false} // if false, year has 2 digits
 ) {
 	return get_ymd_hms(date, 'local', options);
 }
 
 
-export function get_ymd_hms_UTC(date, options = {includeFullYear: false} // if false, year has 2 digits
+export function get_ymd_hms_UTC(
+	date, options = {includeFullYear: false} // if false, year has 2 digits
 ) {
 	return get_ymd_hms(date, 'UTC', options);
 }
